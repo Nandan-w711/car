10c3d1d38a34496420efd06a9bb231a42d34b6b5
Nandan-w711 <braincool329@gmail.com>
2025-07-31 23:24:04 +0600

    Create user.js

---
+++ user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String },
+  password: { type: String, required: true },
+  profilePic: { type: String },
+  dob: { type: Date },
+  gender: { type: String, enum: ['male', 'female', 'other'] },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
